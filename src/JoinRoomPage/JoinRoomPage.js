190baf7de468fa2b03eb9379e4cd75ed18a3bc70
Sensei Stiven <strujillodev@gmail.com>
2024-05-22 23:38:27 -0500

    Dispatch isRoomHost, Display different title depends on the props

--- src/JoinRoomPage/JoinRoomPage.js
+++ src/JoinRoomPage/JoinRoomPage.js
@@ -1,25 +1,43 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-import './JoinRoomPage.css';
+import { connect } from "react-redux";
+import { setIsRoomHost } from "../store/actions";
+import "./JoinRoomPage.css";
+import JoinRoomTitle from "./JoinRoomTitle";
 
 const JoinRoomPage = (props) => {
+  const { setIsRoomHostAction, isRoomHost } = props;
   const search = useLocation().search;
 
   useEffect(() => {
-    const isRoomHost = new URLSearchParams(search).get('host');
+    const isRoomHost = new URLSearchParams(search).get("host");
     if (isRoomHost) {
       // seeting in our redux store that user is host
+      setIsRoomHostAction(true);
     }
   }, []);
 
   return (
-    <div className='join_room_page_container'>
-      <div className='join_room_page_panel'>
-
+    <div className="join_room_page_container">
+      <div className="join_room_page_panel">
+        <JoinRoomTitle isRoomHost={isRoomHost} />
       </div>
     </div>
   );
 };
 
-export default JoinRoomPage;
+const mapStoreStateToProps = (state) => {
+  return {
+    ...state,
+  };
+};
+
+const mapActionsToProps = (dispatch) => {
+  return {
+    setIsRoomHostAction: (isRoomHost) => 
+      dispatch(setIsRoomHost(isRoomHost)),
+  };
+};
+
+export default connect(mapStoreStateToProps, mapActionsToProps)(JoinRoomPage);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,16 @@
+import Actions from "./actions";
+
 const initState = {
   identity: "",
+  isRoomHost: false
 };
 
 const reducer = (state = initState, action) => {
   switch (action.type) {
-    case "DUMMY_ACTION":
+    case Actions.SET_IS_ROOM_HOST:
       return {
         ...state,
+        isRoomHost : action.isRoomHost
       };
     default:
       return state;
