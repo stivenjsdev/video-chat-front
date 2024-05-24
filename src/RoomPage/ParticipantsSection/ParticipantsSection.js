63201f242cef7a8e3d665f0a65bd6321c8fb79df
Sensei Stiven <strujillodev@gmail.com>
2024-05-24 00:40:49 -0500

    Prepare basic structure of room page

---
+++ src/RoomPage/ParticipantsSection/ParticipantsSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ParticipantsSection = () => {
+  return <div>ParticipantsSection</div>;
+};
+
+export default ParticipantsSection;
--- src/RoomPage/RoomPage.js
+++ src/RoomPage/RoomPage.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import ChatSection from './ChatSection/ChatSection';
+import ParticipantsSection from './ParticipantsSection/ParticipantsSection';
+import './RoomPage.css';
+import VideoSection from './VideoSection/VideoSection';
 
 const RoomPage = () => {
   return (
-    <div>RoomPage</div>
+    <div className='room_container'>
+      <ParticipantsSection />
+      <VideoSection />
+      <ChatSection />
+    </div>
   );
 };
 
