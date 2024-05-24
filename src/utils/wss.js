3bc77b944430ec2dfc5f933dbe03e9f825ed6555
Sensei Stiven <strujillodev@gmail.com>
2024-05-24 03:27:36 -0500

    create connection with socket io server

---
+++ src/utils/wss.js
@@ -0,0 +1,14 @@
+import io from "socket.io-client";
+
+const SERVER = "http://localhost:5002";
+
+let socket = null;
+
+export const connectWithScoketIOServer = () => {
+  socket = io(SERVER);
+
+  socket.on("connect", () => {
+    console.log("successfully connected with socket io server");
+    console.log(socket.id);
+  });
+};
