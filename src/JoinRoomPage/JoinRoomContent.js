59f407f44568885bf721f4358a29246325c3b3b2
Sensei Stiven <strujillodev@gmail.com>
2024-05-23 22:44:36 -0500

    create join room page buttons

--- src/JoinRoomPage/JoinRoomContent.js
+++ src/JoinRoomPage/JoinRoomContent.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { setConnectOnlyWithAudio } from "../store/actions";
+import ErrorMessage from "./ErrorMessage";
+import JoinRoomButtons from "./JoinRoomButtons";
 import JoinRoomInputs from "./JoinRoomInputs";
 import OnlyWithAudioCheckbox from "./OnlyWithAudioCheckbox";
 
 const JoinRoomContent = (props) => {
-  const { isRoomHost, setConnectOnlyWithAudioAction, connectOnlyWithAudio } = props;
+  const { isRoomHost, setConnectOnlyWithAudioAction, connectOnlyWithAudio } =
+    props;
 
   const [roomIdValue, setRoomIdValue] = useState("");
   const [nameValue, setNameValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleJoinRoom = () => {
+    //joining the room
+    console.log('joining');
+  }
+
   return (
     <>
       <JoinRoomInputs
@@ -21,6 +31,11 @@ const JoinRoomContent = (props) => {
       <OnlyWithAudioCheckbox
         setConnectOnlyWithAudio={setConnectOnlyWithAudioAction}
         connectOnlyWithAudio={connectOnlyWithAudio}
+      />
+      <ErrorMessage errorMessage={errorMessage} />
+      <JoinRoomButtons
+        handleJoinRoom={handleJoinRoom}
+        isRoomHost={isRoomHost}
       />
     </>
   );
